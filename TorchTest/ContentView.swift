212c6089056d67c45f6183a5dac86decb431702d
Sid Verma <mavidser@gmail.com>
2020-04-15 13:55:45 +0530

    Initial Commit

---
+++ TorchTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TorchTest
+//
+//  Created by Sid Verma on 15/04/20.
+//  Copyright © 2020 Sid Verma. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
